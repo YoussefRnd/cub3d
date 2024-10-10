3810ac07bc5f45cfc3d98dc823c84114750ffcdc
Hassan Brahimi <hbrahimi@c2r2p6.1337.ma>
2024-10-10 11:54:58 +0100

    work in progress

--- src/parser.c
+++ src/parser.c
@@ -6,17 +6,29 @@
 /*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 15:41:06 by hbrahimi          #+#    #+#             */
-/*   Updated: 2024/10/09 16:42:00 by hbrahimi         ###   ########.fr       */
+/*   Updated: 2024/10/10 11:54:46 by hbrahimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/parsing.h"
 
+bool    valid_path(char *path)
+{
+    int len;
+
+    len = ft_strlen(path);
+    if (len < 4)
+        return false;
+    if (!(ft_strcmp(&path[len - 4], ".cub")))
+        return true;
+    return false;
+}
+
 int open_file_and_return_fd(char *path)
 {
     int fd;
 
-    if (!path)
+    if (!valid_path(path))
         return -1;
     fd = open(path, O_RDONLY);
     return fd;
@@ -35,13 +47,17 @@ bool check_validity_of_file(int fd)
 void parse_the_file(char *path)
 {
     int fd;
+    t_booleans those;
 
     fd = open_file_and_return_fd(path);
     if (!check_validity_of_file(fd))
         return ;
 }
 
-bool check_validity_of_map(int fd)
-{
-        
-}
+
+
+
+// bool check_validity_of_map(int fd)
+// {
+    
+// }
--- src/utils.c
+++ src/utils.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/20 15:41:48 by hbrahimi          #+#    #+#             */
-/*   Updated: 2024/10/10 11:52:16 by hbrahimi         ###   ########.fr       */
+/*   Created: 2024/10/10 10:31:55 by hbrahimi          #+#    #+#             */
+/*   Updated: 2024/10/10 10:34:57 by hbrahimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#include "../inc/parsing.h"
 
-# include "../get_next_line/get_next_line.h"
-# include "../libft/libft.h"
-# include <fcntl.h>
-# include <stdbool.h>
-
-typedef struct s_booleans_list
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	bool	floor_color;
-	bool	ceiling_color;
-	bool	west_texture;
-	bool	east_texture;
-    bool    north_texture;
-    bool    south_texture;
-}			t_booleans;
-
-int			ft_strcmp(const char *s1, const char *s2);
-
-#endif
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
